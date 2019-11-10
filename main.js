60ef6bee45f1e15c0abae5d7623692c509af86cc
tom-milner <tom.f.milner@gmail.com>
2019-11-10 22:56:21 +0000

    Finished

--- main.js
+++ main.js
@@ -7,90 +7,91 @@ canvas.height = window.innerHeight;
 
 // create elements
 const graph = {
-  "A": ["B", "C"],
+  "A": ["B", "C", "D", "E"],
   "B": [],
   "C": [],
-  "D": ["A"]
+  "D": [],
+  "E": ["F", "G"],
+  "F": [],
+  "G": []
 }
 
 // render nodes
-for (node in graph) {
-  addNode(node);
-}
+addNodes();
 
 // connect nodes
 drawEdges();
 
 
-// search algorithm
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 // Rendering Graph
 function drawEdges() {
+  // remove weights
+  document.querySelectorAll(".weight").forEach(el => el.remove())
   ctx.clearRect(0, 0, canvas.width, canvas.height);
+
   for (node in graph) {
     let el = document.querySelector(`#${node}`);
     let elCoords = getElCoords(el);
     console.log("node: ", node);
+
+
 
     graph[node].forEach(neighbour => {
       ctx.beginPath();
       console.log("neighbour: ", neighbour)
       let nEl = document.querySelector(`#${neighbour}`);
       nCoords = getElCoords(nEl);
-      console.log(nCoords);
-      console.log(elCoords);
+
       ctx.moveTo(elCoords.x, elCoords.y);
       ctx.lineTo(nCoords.x, nCoords.y)
       ctx.stroke();
-
+      drawMagnitude(elCoords, nCoords);
     });
   }
+}
+
+function drawMagnitude(coords1, coords2) {
+
+  let dX = coords2.x - coords1.x;
+  let dY = coords2.y - coords1.y;
+  let mag = Math.sqrt(Math.pow(dX, 2) + Math.pow(dY, 2));
+
+  let textX = coords1.x + dX / 2;
+  let textY = coords1.y + dY / 2;
+
+  var textEl = document.createElement("div");
+  var text = document.createTextNode(Math.floor(mag) / 10);
+  textEl.appendChild(text);
+  textEl.className = "weight";
+  console.log(textX, textY);
+  textEl.style.left = `${textX}px`;
+  textEl.style.top = `${textY}px`;
+
+  document.body.appendChild(textEl);
 }
 
 function getElCoords(el) {
   let rect = el.getBoundingClientRect();
   return {
-    x: rect.x + rect.width / 2,
-    y: rect.y + rect.height / 2
+    x: rect.x + (rect.width / 2),
+    y: rect.y + (rect.height / 2)
   }
 }
 
-function addNode(node) {
-  var el = document.createElement("div");
-  var text = document.createTextNode(node);
-  el.appendChild(text);
-  el.className = "node";
-  el.setAttribute("id", node);
-  let rX = Math.random() * window.innerWidth;
-  let rY = Math.random() * window.innerHeight;
-  el.style.left = `${rX}px`
-  el.style.top = `${rY}px`
-  document.body.appendChild(el);
+function addNodes() {
+  for (node in graph) {
+    var el = document.createElement("div");
+    var text = document.createTextNode(node);
+    el.appendChild(text);
+    el.className = "node";
+    el.setAttribute("id", node);
+    let rX = 100 + Math.random() * (window.innerWidth - 200);
+    let rY = 100 + Math.random() * (window.innerHeight - 200);
+    el.style.left = `${rX}px`
+    el.style.top = `${rY}px`
+    document.body.appendChild(el);
+  }
 }
 
 
@@ -109,11 +110,11 @@ function grabElement(event) {
 };
 
 
-onmousemove  = function (event) {
+onmousemove = function (event) {
   if (isGrabbed) {
     let rect = currEl.getBoundingClientRect();
-    currEl.style.left = `${event.clientX  - rect.width/2}px`;
-    currEl.style.top = `${event.clientY  - rect.height/2}px`;
+    currEl.style.left = `${event.clientX -rect.width/2}px`;
+    currEl.style.top = `${event.clientY - rect.height/2 }px`;
     drawEdges();
   }
 }
